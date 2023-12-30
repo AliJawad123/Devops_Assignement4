8bfd983dbaef2315f3f7693b0a073cba5168d898
Umer Riaz <96389742+Umerriaz111@users.noreply.github.com>
2023-12-31 00:42:41 +0500

    Delete third_task_keypair.tf

--- third_task_keypair.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "N.Virginia"
-}
-
-resource "aws_key_pair" "assignment4_key" {
-  key_name   = "cs423-assignment4-key"
-  public_key = file("/Users/umerriaz/Desktop/Keys/Keys_For_First_Instance.pem")  # Replace with the path to your public key file
-}
-
